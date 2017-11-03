f0b0520843d28cb07a65013b1745ec5d3abe9d75
liqinghong <liqinghong@jianke.com>
2017-11-03 16:35:24 +0800

    更改

---
+++ JKTestdd.framework/Headers/JKTestdd.h
@@ -0,0 +1,19 @@
+//
+//  JKTestdd.h
+//  JKTestdd
+//
+//  Created by liqinghong123 on 2017/11/3.
+//  Copyright © 2017年 liqinghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKTestdd.
+FOUNDATION_EXPORT double JKTestddVersionNumber;
+
+//! Project version string for JKTestdd.
+FOUNDATION_EXPORT const unsigned char JKTestddVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKTestdd/PublicHeader.h>
+
+
